f797bfc07e22aefc631a5c65ebe66eb59e612512
Bertrand Atangana Ekobo <129292669+bertrand12345@users.noreply.github.com>
2024-07-09 12:55:40 +0000

    third commit

--- hellocli.py
+++ hellocli.py
@@ -6,7 +6,7 @@ from hello import marco
 @click.option("--name")
 def callmarco(name):
     result = marco(name)
-    click.echo(click.style(f'{result}', bg='blue', fg='white')
+    click.echo(click.style(f'{result}', bg='blue', fg='white'))
 
 if __name__ == '__main__':
     #pylint: disable=no-value-for-parameter
