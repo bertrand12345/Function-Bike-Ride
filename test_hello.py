06367ede64eec5322c39d035350ac842cdc6899b
Bertrand Atangana Ekobo <129292669+bertrand12345@users.noreply.github.com>
2024-07-11 10:54:29 +0000

    adding fully tested application

--- test_hello.py
+++ test_hello.py
@@ -9,8 +9,14 @@ def test_no_marco():
     assert "No!" == marco("Bob")
 
 
-def test_search():
+def test_not_marco_cli():
     runner = CliRunner()
     result = runner.invoke(callmarco, ["--name", "Bob"])
     assert result.exit_code == 0
     assert 'No!' in result.output
+
+def test_marco_cli():
+    runner = CliRunner()
+    result = runner.invoke(callmarco, ["--name", "Marco"])
+    assert result.exit_code == 0
+    assert 'Polo' in result.output
